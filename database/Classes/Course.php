670f0bd55d8e7620ad595157cb10395c6eb9ed69
Alex Northouse <ajnorthouse@bsu.com>
2020-02-09 16:18:52 -0500

    Finish course class code

--- database/Classes/Course.php
+++ database/Classes/Course.php
@@ -1,34 +1,41 @@
 <?php
 	
-	class Question
+	class Course
 	{
 		// Class variables, only accessible through getters
 		private int $ID;
-		private int $crseID;
-		private string $qText;
-		private string $aText;
+		private int $uniID;
+		private string $crseID;
+		private int $sectNum;
+		private string $crseName;
 		private array $userRoster;
 
 		// constructor method
-		public function __construct(int $ID, int $crseID, string $qText, string $aText)
+		public function __construct(int $ID, int $uniID, string $crseID, int $sectNum, string $crseName)
 		{
 			$this->ID		= $ID;
+			$this->uniID	= $uniID;
 			$this->crseID	= $crseID;
-			$this->qText	= $qText;
-			$this->aText	= $aText;
+			$this->sectNum	= $sectNum;
+			$this->crseName	= $crseName;
+			$this->userRoster	= DatabaseMethods::getUserRoster($ID);
 		}
 
 		// getters
 		public function getID(): int { return $this->ID; }
-		public function getCrseID(): int { return $this->crseID; }
-		public function getQText(): string { return $this->qText; }
-		public function getAText(): string { return $this->aText; }
+		public function getuniID(): int { return $this->uniID; }
+		public function getcrseID(): string { return $this->crseID; }
+		public function getsectNum(): int { return $this->sectNum; }
+		public function getcrseName(): string { return $this->crseName; }
+		public function getuserRoster(): array { return $this->userRoster; }
 
 		// setters
-		public function setID(int $ID) {  $this->ID = $ID; }
-		public function setCrseID(int $crseID) {  $this->crseID = $crseID; }
-		public function setQText(string $qText) {  $this->qText = $qText; }
-		public function setAText(string $aText) {  $this->aText = $aText; }
+		//	 there's no "setID" because that would compromise the database's integrity
+		public function setuniID(int $uniID) {  $this->uniID = $uniID; }
+		public function setcrseID(string $crseID) {  $this->crseID = $crseID; }
+		public function setsectNum(int $sectNum) {  $this->sectNum = $sectNum; }
+		public function setcrseName(string $crseName) {  $this->crseName = $crseName; }
+		public function setuserRoster(array $userRoster) {  $this->userRoster = $userRoster; }
 	}
 
 ?>
