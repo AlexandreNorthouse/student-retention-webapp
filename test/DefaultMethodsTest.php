5086058f756391453003b0c59b62e1229406623c
brendenjones12 <brendenjones4320@gmail.com>
2020-02-17 16:38:03 -0500

    Fixed meht to meth

--- test/DefaultMethodsTest.php
+++ test/DefaultMethodsTest.php
@@ -1,6 +1,6 @@
 <?php
     declare(strict_types=1);
-    require_once(dirname(__FILE__, 2) . "/logic/Default_Mehtods.php");
+    require_once(dirname(__FILE__, 2) . "/logic/Default_Methods.php");
 
     use PHPUnit\Framework\TestCase;
 
