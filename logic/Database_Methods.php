185fc58980fa8f61342a472edcd12677e36d888c
Alex Northouse <ajnorthouse@bsu.com>
2020-02-17 15:38:26 -0500

    Create default logic and database methods

--- logic/Database_Methods.php
+++ logic/Database_Methods.php
@@ -2,4 +2,197 @@
 
 	require_once( dirname(__FILE__, 2) . "\database\Database_Connection.php");
 
+	class DatabaseMethods {
+
+	    // default method layout:
+        /*
+
+        // [description of what the method does]
+        public static function asdf(asdf $asdf): asdf {
+            // creates SQL query and attempts to execute it
+            try {
+                $query = "";
+                $sql = $conn->prepare($query);
+                $sql->execute();
+                $class = $sql->fetchAll();
+
+                if ()
+                    return FALSE;
+
+            // if something goes wrong, the code below executes as a failsafe
+            } catch (Exception $e) {
+
+            }
+
+            // if nothings goes wrong and ..., it returns true
+            return TRUE;
+        }
+        */
+
+
+
+        // Universally used methods
+        // [description of what the method does]
+        public static function getEnrolledCourses($userID): array
+        {
+            /*
+            $query = "SELECT Class.classID, Class.crseID, Class.sectNum, Class.crseName FROM Class, ClassUserRoster WHERE ClassUserRoster.classID=Class.classID AND ClassUserRoster.username='$username'";
+            $sql = $conn->prepare($query);
+            $sql->execute();
+            $classList = $sql->fetchAll();
+            */
+            return array();
+        }
+
+
+
+        // Add_Course_Methods database methods
+        // [description of what the method does]
+		public static function checkCourseExists(int $courseNumber): bool
+        {
+			// creates SQL query and attempts to execute it
+			try {
+				$query = "SELECT classID FROM Class WHERE classID=$courseNumber";
+                $sql = $conn->prepare($query);
+				$sql->execute();
+				$class = $sql->fetchAll();
+
+				if (empty($class))
+					return FALSE;
+
+			// if something goes wrong, the code below executes as a failsafe
+			} catch (Exception $e) {
+				return FALSE;
+			}
+
+			// if nothing goes wrong and the course exists, it returns true
+			return TRUE;
+		}
+
+        // [description of what the method does]
+        public static function checkEnrollment(int $courseNumber, int $userID): bool
+        {
+            /*
+			$query = "SELECT * FROM * WHERE ";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+        // [description of what the method does]
+        public static function attemptStudentInsertion(int $courseNumber, int $userID): bool
+        {
+            /*
+			$query = "SELECT * FROM * WHERE ";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+
+
+        // Chatbot_Methods database methods
+        // This is where a method would go... IF IT HAD ONE!
+
+
+
+        // Add_Data_Methods database methods
+        // [description of what the method does]
+        public static function duplicateQACheck(String $question, String $answer, int $selectedCourse): bool
+        {
+            /*
+			$query = "SELECT * FROM Question, ClassQuestions WHERE
+						ClassQuestions.quesID=$selectedClass AND Question.qtext='$question'";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+        // [description of what the method does]
+        public static function attemptQAInsertion(String $question, String $answer, int $selectedCourse): bool
+        {
+            /*
+			$query = "SELECT * FROM * WHERE ";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+
+
+        // Create_Course_Methods database methods
+        // [description of what the method does]
+        public static function duplicateCrseNumSectCheck(int $courseNumber, String $courseSection, String $universityID): bool
+        {
+            /*
+			$query = "SELECT * FROM UniversityClassRoster, Class WHERE Class.crseID='$classNum' AND Class.sectNum=$classSec AND UniversityClassRoster.uniID=$uniID";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$classIDs = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+        // [description of what the method does]
+        public static function attemptCourseInsertion(int $courseNumber, String $courseSection, String $courseName, String $universityID, String $professorID): bool
+        {
+            /*
+			$query = "SELECT * FROM * WHERE ";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+
+
+        // Create_Syllabus_Methods database methods
+        // [description of what the method does]
+        public static function checkSyllabusExistsint ($courseID)
+        {
+            /*
+			$query = "SELECT * FROM * WHERE ";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return TRUE;
+        }
+
+        // [description of what the method does]
+        public static function attemptSyllabusInsertion(int $courseID, String $courseTitle, String $contactInfo, String $officeHours, String $courseDesc, String $courseGoals, String $reqMaterials, String $grading, String $attendance, String $uniPolicies, String $stuResources)
+        {
+            /*
+            $query = "INSERT INTO Syllabus VALUES (NULL, '$ct', '$ci', '$ohp', '$cd', '$cg', '$rm',
+                        '$g', '$a', '$up', '$sr')";
+            $sql = $conn->prepare($query);
+            $sql->execute();
+            */
+            return TRUE;
+        }
+
+
+
+        // View_Data_Methods database methods
+        // [description of what the method does]
+        public static function attemptQuestionsPull($courseNumber): array
+        {
+            /*
+			$query = "SELECT * FROM * WHERE ";
+			$sql = $conn->prepare($query);
+			$sql->execute();
+			$copyCheck = $sql->fetchAll();
+            */
+            return array();
+        }
+    }
 ?>
--- logic/Default_Methods.php
+++ logic/Default_Methods.php
@@ -1,10 +1,77 @@
 <?php
+
+	class DefaultMethods {
+
+		// logs out the user
+		public static function logoutUser() {
+			session_start();
+			session_destroy();
+			header('Location: index.php');
+		}
+
+		// This will check the login of the current user, using the $_SESSION variable
+		public static function checkLogin(string $userType) {
+			// makes sure the crucial variable is set before continuing
+			if (isset($_SESSION['isProf'])){
+				
+				// student login check
+				if ($userType == "Student" && $_SESSION['isProf'] != FALSE) {
+					DefaultMethods::logoutUser();
+				}
+
+				// professor login check
+				if ($userType == "Professor" && $_SESSION['isProf'] != TRUE) {
+					DefaultMethods::logoutUser();
+				}
+			} else {
+				DefaultMethods::logoutUser();
+			}
+		}
+
+		// This will format all the given fields in a dictionary array
+		public static function formatFields(array $inputs): array {
+			// creates array to collect any generated errors
+			$errors = array();
+
+			// runs through all the inputs to format them and check for empty values
+			foreach ($inputs as $inputName => &$value) {
+
+				// first trims the value
+				$value = trim($value);
+		
+				// then reports if its empty
+				if (empty($value) || $value == "") {
+					$errors[] = "The $inputName field can't be empty!";
+				}
+			}
+			
+			// then returns an arrays of errors or...
+			if (!empty($errors))
+				return DefaultMethods::generateReturnArray("Error", $errors);
+			
+			// a successful empty array
+			return DefaultMethods::generateReturnArray();
+			
+		}
+
+		// This handles returning the array needed for the phtml pages to interpret the results.
+		public static function generateReturnArray(string $outcome = null, array $feedback = null): array {
+			// populates a return array if the function is sent two values
+			if (isset($outcome) && isset($feedback))
+				return array("Outcome" => $outcome, "Feedback"=> $feedback);
+			
+			// otherwise returns an empty array
+			return array();
+		}
+
+		// gets an array of all the courses a user is enrolled in
+        public static function getEnrolledCourses(): array
+        {
+            // sets variables for more legible names
+            $userID = $_SESSION['UserID'];
+            DatabaseMethods::getEnrolledCourses($userID);
+        }
+
+    }
 	
-	// Placeholder for future changes
-
-	// logout method
-		/*session_start();
-		session_destroy();
-		header('Location: index.php');*/
-
 ?>
