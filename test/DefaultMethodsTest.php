8feacb4df61d855aa5465d296b6e62862635bf23
brendenjones12 <brendenjones4320@gmail.com>
2020-02-17 16:23:54 -0500

    Add new Test Methods

--- test/DefaultMethodsTest.php
+++ test/DefaultMethodsTest.php
@@ -1,6 +1,6 @@
 <?php
     declare(strict_types=1);
-    //require_once();
+    require_once(dirname(__FILE__, 2) . "/logic/Default_Mehtods.php");
 
     use PHPUnit\Framework\TestCase;
 
@@ -8,13 +8,21 @@
     class DefaultMethodsTest extends TestCase
     {
 
-        public function testGenerateReturnArray()
-        {
-            $this->assertEquals(0, 0, "They're the same!");
-        }
-
         public function testFormatFields()
         {
+            $testArray = array(
+                "Test" => "   test   "
+            );
+
+            $correctArray = array(
+                "Test" => "test"
+            );
+            $this->assertEquals(defaultMethods::formatFields($testArray), $correctArray, "They're the same!");
+        }
+
+        public function testGenerateReturnArray()
+        {
+            
             $this->assertNotEquals(0, 0, "They're not the same!");
         }
     }
