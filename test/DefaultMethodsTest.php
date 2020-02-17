04f8ae935f3ccb4c849d54c14c11fc97ee4ef9e0
Alex Northouse <ajnorthouse@bsu.com>
2020-02-17 15:39:59 -0500

    Create example PHPUnit test file

---
+++ test/DefaultMethodsTest.php
@@ -0,0 +1,20 @@
+
+declare(strict_types=1);
+require_once();
+
+use PHPUnit\Framework\TestCase;
+
+
+class DefaultMethodsTest extends TestCase
+{
+
+    public function testGenerateReturnArray()
+    {
+        $this->assertEquals(0, 0, "They're the same!");
+    }
+
+    public function testFormatFields()
+    {
+        $this->assertNotEquals(0, 0, "They're not the same!");
+    }
+}
