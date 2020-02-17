499c2cbc53fc4953b49a6afd6001ec93a21e59cb
Alex Northouse <ajnorthouse@bsu.com>
2020-02-17 15:36:19 -0500

    Separate presentation and logic

---
+++ logic/Professor/Add_Data_Methods.php
@@ -0,0 +1,100 @@
+<?php
+    // this makes sure that all stored session values are kept
+    session_start();
+
+    // this includes the needed database and default methods
+    require_once( dirname(__FILE__, 3) . "\logic\Database_Methods.php" );
+    require_once( dirname(__FILE__, 3) . "\logic\Default_Methods.php" );
+
+    $classList = DefaultMethods::getEnrolledCourses();
+
+    // this handles calling the logic function and its return array
+    if (!empty($_POST)) {
+        $feedback = AddDataMethods::addQuestion();
+    } else {
+        $feedback = array();
+    }
+
+
+    class AddDataMethods {
+
+        // main function called by presentation layer
+        public static function addQuestion(): array
+        {
+            DefaultMethods::checkLogin('Professor');
+
+
+            // this sets the variables needed for this method.
+            $feedback = array();
+            $inputArray = array(
+                "Question" => ($_POST['dataQuestion']),
+                "Answer" => ($_POST['dataAnswer']),
+                "Selected Class" => ($_POST['dataClassID'])
+            );
+
+
+            // this formats the fields, returns false if at least 1 field is empty
+            $feedback = DefaultMethods::formatFields($inputArray);
+            if (isset($feedback["Outcome"]))
+                return $feedback;
+
+
+            // this checks to see if the course already has that question and answer combo
+            $feedback = AddDataMethods::duplicateQACheck($inputArray);
+            if (isset($feedback["Outcome"]))
+                return $feedback;
+
+
+            // this inserts the question/answer into the database
+            $feedback = AddDataMethods::attemptQAInsertion($inputArray);
+            if (isset($feedback["Outcome"]))
+                return $feedback;
+
+
+            // if all went according to plan, it will return a success feedback
+            $success = array("The question was successfully added to the course!");
+            return DefaultMethods::generateReturnArray("Success", $success);
+        }
+
+
+
+        // [description of what the method does]
+        private static function duplicateQACheck(array $inputArray): array
+        {
+            // sets variables for more legible variable names
+            $question = $inputArray['Question'];
+            $answer = $inputArray['Answer'];
+            $selectedCourse = $inputArray['Selected Course'];
+
+
+            // duplicate question check error code
+            if (!DatabaseMethods::duplicateQACheck($question, $answer, $selectedCourse)) {
+                $errorArray = array("It seems like that question / answer combo already exists, please contact your system admin with this error.");
+                return DefaultMethods::generateReturnArray("Error", $errorArray);
+            }
+
+            // duplicate question check success code
+            return DefaultMethods::generateReturnArray();
+        }
+
+        // [description of what the method does]
+        private static function attemptQAInsertion(array $inputArray): array
+        {
+            // sets variables for more legible variable names
+            $question = $inputArray['Question'];
+            $answer = $inputArray['Answer'];
+            $selectedCourse = $inputArray['Selected Course'];
+
+            // attempted insertion error code
+            if (!DatabaseMethods::attemptQAInsertion($question, $answer, $selectedCourse)) {
+                $errorArray = array("It seems like something went wrong on our end, please try again in a couple of moments or contact your systems admin!");
+                return DefaultMethods::generateReturnArray("Error", $errorArray);
+            }
+
+            // attempted insertion success code
+            return DefaultMethods::generateReturnArray();
+        }
+
+    }
+
+?>
--- public/Professor/Add_Data.php
+++ public/Professor/Add_Data.php
@@ -1,97 +1,13 @@
 <?php
-	session_start();
-	
-	//This makes sure that a professor is logged in
-	if($_SESSION['isProf'] != 1 || empty($_SESSION)) {
-		//This will send the user away if there's no student login info
-		header('Location: index.php');
-	}
-	
-	//This sets all the universal variables
-	include_once 'system-connect.php';
-	$error = array();
-	$success = array();
-	$username = $_SESSION['username'];
-	
-	// this is so the section object can be created
-	$query = "SELECT Class.classID, Class.crseID, Class.sectNum, Class.crseName FROM Class, ClassUserRoster WHERE ClassUserRoster.classID=Class.classID AND ClassUserRoster.username='$username'";
-	$sql = $conn->prepare($query);
-	$sql->execute();
-	$classList = $sql->fetchAll();
-	
-	
-	// This handles the "Submit New Question" button being hit
-	if (!empty($_POST) && !empty($_POST['submitData'])){
-		$question = trim($_POST['dataQuestion']);
-		$answer = trim($_POST['dataAnswer']);
-		$selectedClass = intval($_POST['dataClassID']);
-		
-		
-		// checks for empty fields first
-		if (empty($question) || $question == "") {
-			$error[] = "The Question field can't be empty!";
-		}
-		if (empty($answer) || $answer == "") {
-			$error[] = "The Answer field can't be empty!";
-		}
-		
-		// checks for no errors before then doing a duplicate question and answer check
-		if (empty($error)) {
-			$query = "SELECT * FROM Question, ClassQuestions WHERE 
-						ClassQuestions.quesID=$selectedClass AND Question.qtext='$question'";
-			$sql = $conn->prepare($query);
-			$sql->execute();
-			$copyCheck = $sql->fetchAll();
-			
-			if (!empty($copyCheck)){
-				$error[] = "That question already exists for that class!";
-			}
-		}
-		
-		// does one final error check before finaly inserting the data into the database
-		if (empty($error)) {
-			// This creates the class in the class table
-			$query = "INSERT INTO Question VALUES (NULL, '$question', '$answer')";
-			$sql = $conn->prepare($query);
-			$sql->execute();
-			
-			
-			// This then gets that question's ID
-			$query = "SELECT LAST_INSERT_ID()";
-			$sql = $conn->prepare($query);
-			$sql->execute();
-			$questionCheck = $sql->fetchAll();
-			$questionID = $questionCheck[0]['LAST_INSERT_ID()'];
-			
-			
-			// This then inserts the relationship entry into ClassQuestions
-			$query = "INSERT INTO ClassQuestions VALUES ($selectedClass, $questionID)";
-			$sql = $conn->prepare($query);
-			$sql->execute();
-			
-			
-			// finally, there's a success statement given and all the values are cleared
-			$success[] = "Question successfully added to that class!";
-			$question  = "";
-			$answer = "";
-			$selectedClass = "";
-		}
-	} else {
-		$selectedClass = 0;
-		$results = [];
-	}
+    require_once( dirname(__FILE__, 3) . "\logic\Professor\Add_Data_Methods.php");
 ?>
-
-
 
 <html>
   <head>
 	<title>Professor - Add Data</title>
-	<link rel="stylesheet" href="StyleSheet_Sidebar.css">
-	<link rel="stylesheet" href="StyleSheet_Professor.css">
+      <link rel="stylesheet" href="../StyleSheets/StyleSheet_Sidebar.css">
+      <link rel="stylesheet" href="../StyleSheets/StyleSheet_Professor.css">
   </head>
-  
-  
   <body>
   	<div class="sidebar">
 	  <a href="prof-viewData.php">View Data</a>
@@ -103,16 +19,15 @@
 	
 	<div class="content">
 		<h2>Add Question</h2>
-		
-		
 		<form action="" method="post">
 			<label for="dataClassID">Select Course: </label><br>
 			<select name="dataClassID">
 				<?php
+                // this displays the enrolled courses in a dropdown menu
 					if (!empty($classList)){
 						foreach($classList as $c){
 							echo ('<option value="' . $c['classID'] . '"');
-							if ($selectedClass == $c['classID']) {
+							if ($_POST['dataClassID'] == $c['classID']) {
 								echo (" selected ");
 							}
 							echo ('>' . $c['crseName'] . '</option>');
@@ -123,19 +38,25 @@
 			<br><br>
 			
 			<label for="dataQuestion">Question: </label><br>
-			<textarea name="dataQuestion" required value="<?php if(!empty($_POST['dataQuestion'])){ echo $question; } else { echo ''; } ?>"></textarea>
+			<textarea name="dataQuestion" required value="<?php if(!empty($_POST['dataQuestion'])){ echo $_POST['dataQuestion']; } else { echo ''; } ?>"></textarea>
 			<br><br>
 			
 			<label for="dataAnswer">Answer: </label><br>
-			<textarea name="dataAnswer" required value="<?php if(!empty($_POST['dataAnswer'])){ echo $answer; } else { echo ''; } ?>"/></textarea>
+			<textarea name="dataAnswer" required value="<?php if(!empty($_POST['dataAnswer'])){ echo $_POST['dataAnswer']; } else { echo ''; } ?>"/></textarea>
 			<br><br>
 			
 			<button type="submit" name="submitData" value="✓">Create New Question</button>
 			<br><br>
 		</form>
-		
-		<span class="error"><?php if(!empty($error)) foreach($error as $e) echo $e . "<br>"; ?></span>
-		<span class="success"><?php if(!empty($success)) foreach($success as $s) echo $s . "<br>"; ?></span>
+
+        <?php
+        // this displays the feedback from the logic method
+            if (!empty($feedback["Feedback"])) {
+                echo("<span class=\"". $feedback["Outcome"] . "\">");
+                foreach($feedback["Feedback"] as $a) echo $a . "<br>";
+                echo("</span>");
+            }
+        ?>
 	</div>
   </body>
 </html>
