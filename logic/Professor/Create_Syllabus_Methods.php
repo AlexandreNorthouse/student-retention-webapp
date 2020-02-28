acee0e1acb921aeba9c33792bc735d9196033b64
Alex Northouse <ajnorthouse@bsu.com>
2020-02-28 08:59:46 -0500

    Add new syllabus functionality

--- logic/Professor/Create_Syllabus_Methods.php
+++ logic/Professor/Create_Syllabus_Methods.php
@@ -10,7 +10,19 @@
     $classList = DefaultMethods::getEnrolledCourses();
 
     // this handles calling the logic function and its return array
-    if (!empty($_POST)) {
+    if (!empty($_POST['getSyllabus'])) {
+        $inputArray = array(
+            "Course ID" => $_POST['courseID']
+        );
+
+        $tempArray = CreateSyllabusMethods::getSyllabus($inputArray);
+        if (!empty($tempArray)) {
+            $syllabusArray = CreateSyllabusMethods::getSyllabus($inputArray)[0];
+        } else {
+            $syllabusArray = $tempArray;
+        }
+    }
+    else if (!empty($_POST['submitSyllabus'])) {
         $inputArray = array(
             "Course ID"           => $_POST['courseID'],
             "Course Title"        => $_POST['courseTitle'],
@@ -24,16 +36,60 @@
             "University Policies" => $_POST['uniPolicies'],
             "Student Resources"   => $_POST['stuResources']
         );
+
         $feedback = CreateSyllabusMethods::createSyllabus($inputArray);
-    } else {
-        $feedback = array();
+
+        $tempArray = CreateSyllabusMethods::getSyllabus($inputArray);
+        if (!empty($tempArray)) {
+            $syllabusArray = CreateSyllabusMethods::getSyllabus($inputArray)[0];
+        } else {
+            $syllabusArray = $tempArray;
+        }
+    }
+    else if (!empty($_POST['updateSyllabus'])){
+        $inputArray = array(
+            "Course ID"           => $_POST['courseID'],
+            "Course Title"        => $_POST['courseTitle'],
+            "Contact Information" => $_POST['contactInfo'],
+            "Office Hours"        => $_POST['officeHours'],
+            "Course Description"  => $_POST['courseDes'],
+            "Course Goals"        => $_POST['courseGoals'],
+            "Required Materials"  => $_POST['reqMaterials'],
+            "Grading Policy"      => $_POST['gradingPolicy'],
+            "Attendance Policy"   => $_POST['attenPolicy'],
+            "University Policies" => $_POST['uniPolicies'],
+            "Student Resources"   => $_POST['stuResources']
+        );
+
+        $feedback = CreateSyllabusMethods::updateSyllabus($inputArray);
+
+        $tempArray = CreateSyllabusMethods::getSyllabus($inputArray);
+        if (!empty($tempArray)) {
+            $syllabusArray = CreateSyllabusMethods::getSyllabus($inputArray)[0];
+        } else {
+            $syllabusArray = $tempArray;
+        }
+    }
+    else if (!empty($_POST['deleteSyllabus'])){
+        $inputArray = array(
+            "Course ID"           => $_POST['courseID']
+        );
+
+        $feedback = CreateSyllabusMethods::attemptSyllabusDelete($inputArray);
+
+        $tempArray = CreateSyllabusMethods::getSyllabus($inputArray);
+        if (!empty($tempArray)) {
+            $syllabusArray = CreateSyllabusMethods::getSyllabus($inputArray)[0];
+        } else {
+            $syllabusArray = $tempArray;
+        }
     }
 
 
     class CreateSyllabusMethods {
 
         // main function called by presentation layer
-        public static function createSyllabus($inputArray): array
+        public static function createSyllabus(array $inputArray): array
         {
             // this sets the variables needed for this method.
             $feedback = array();
@@ -41,12 +97,6 @@
 
             // this formats the fields, returns false if at least 1 field is empty
             $feedback = DefaultMethods::formatFields($inputArray);
-            if (isset($feedback["Outcome"]))
-                return $feedback;
-
-
-            // Checks to see if a syllabus already exists for the selected course
-            $feedback = CreateSyllabusMethods::checkSyllabusExists($inputArray);
             if (isset($feedback["Outcome"]))
                 return $feedback;
 
@@ -63,22 +113,37 @@
             return DefaultMethods::generateReturnArray("Success", $success);
         }
 
+        public static function updateSyllabus(array $inputArray): array
+        {
+            // this sets the variables needed for this method.
+            $feedback = array();
 
 
-        // Checks database for a syllabus with that course ID; returns empty array if it exists, error array otherwise.
-        public static function checkSyllabusExists(array $inputArray): array
+            // this formats the fields, returns false if at least 1 field is empty
+            $feedback = DefaultMethods::formatFields($inputArray);
+            if (isset($feedback["Outcome"]))
+                return $feedback;
+
+
+            // attempts to get all the questions related to the course selected
+            $feedback = CreateSyllabusMethods::attemptSyllabusUpdate($inputArray);
+            if (isset($feedback["Outcome"]))
+                return $feedback;
+
+
+            // if all went according to plan, it will return a success feedback
+            $successArray = array("Syllabus successfully updated!");
+            return DefaultMethods::generateReturnArray("Success", $successArray);
+        }
+
+
+
+        // Attempts to get a syllabus; returns content array for success, empty array otherwise.
+        public static function getSyllabus(array $inputArray):array
         {
             // sets variables for more legible variable names
-            $courseID = $inputArray['Course ID'];
-
-            // attempted insertion error code
-            if (DatabaseMethods::checkSyllabusExists($courseID)) {
-                $errorArray = array("It seems like a syllabus already exists for this course!");
-                return DefaultMethods::generateReturnArray("Error", $errorArray);
-            }
-
-            // attempted insertion success code
-            return DefaultMethods::generateReturnArray();
+            $courseID   = $inputArray['Course ID'];
+            return DatabaseMethods::checkSyllabusExists($courseID);
         }
 
         // Attempts to insert row into syllabi; returns empty array for success, error array otherwise
@@ -108,6 +173,53 @@
             // attempted insertion success code
             return DefaultMethods::generateReturnArray();
         }
+
+        // Attempts to update a question; returns blank array on success, error array otherwise.
+        public static function attemptSyllabusUpdate(array $inputArray): array
+        {
+            // sets variables for more legible variable names
+            $courseID     = $inputArray['Course ID'];
+            $title        = $inputArray['Course Title'];
+            $contactInfo  = $inputArray['Contact Information'];
+            $officeHours  = $inputArray['Office Hours'];
+            $description  = $inputArray['Course Description'];
+            $goals        = $inputArray['Course Goals'];
+            $materials    = $inputArray['Required Materials'];
+            $grading      = $inputArray['Grading Policy'];
+            $attendance   = $inputArray['Attendance Policy'];
+            $uniPolicies  = $inputArray['University Policies'];
+            $resources    = $inputArray['Student Resources'];
+
+            // attempted insertion error code
+            if (!DatabaseMethods::attemptSyllabusUpdate($courseID, $title, $contactInfo, $officeHours,
+                $description, $goals, $materials, $grading, $attendance, $uniPolicies, $resources)) {
+                $errorArray = array("It seems like something went wrong while updating the syllabus, please "
+                    . "try again in a couple of moments or contact your systems admin!");
+                return DefaultMethods::generateReturnArray("Error", $errorArray);
+            }
+
+            // attempted insertion success code
+            return DefaultMethods::generateReturnArray();
+        }
+
+        // Attempts to update a question; returns success array on success, error array otherwise.
+        public static function attemptSyllabusDelete(array $inputArray): array
+        {
+            // sets variables for more legible variable names
+            $courseID   = $inputArray['Course ID'];
+
+            // attempted insertion error code
+            if (!DatabaseMethods::attemptSyllabusDeletion($courseID)) {
+                $errorArray = array("Something went wrong updating the question, contact your system admin"
+                    . " for help!");
+                return DefaultMethods::generateReturnArray("Error", $errorArray);
+            }
+
+            // attempted insertion success code
+            $successArray = array("Question successfully deleted!");
+            return DefaultMethods::generateReturnArray("Success", $successArray);
+        }
+
     }
 
 ?>
--- public/Professor/Create_Syllabus.php
+++ public/Professor/Create_Syllabus.php
@@ -10,10 +10,12 @@
     </head>
     <body>
         <div class="sidebar">
-            <a href="View_Data.php">View Data</a>
+            <a href="View_Data.php">View Questions</a>
             <a href="Add_Data.php">Add Questions</a>
             <a class="active" href="Create_Syllabus.php">Create Syllabus</a>
             <a href="Create_Course.php">Create a Course</a>
+            <a href="View_Courses.php">View Created Courses</a>
+            <a href="Student_Enrollment.php">Modify Student Enrollment</a>
             <a class="bottom" href="../../logic/Logout_User.php">Logout</a>
         </div>
 
@@ -37,6 +39,9 @@
                     }
                     ?>
                 </select>
+                <br><br>
+
+                <button type="submit" name="getSyllabus" value="✓">Get Course's Syllabus</button>
 
 
                 <br><br>
@@ -50,68 +55,66 @@
                 ?>
                 <br>
 
-                <label for="courseTitle">Course Title: </label><br>
-                <textarea name="courseTitle" required><?php
-                    if(!empty($_POST['courseTitle'])){ echo $_POST['courseTitle']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
+                <?php
+                if (isset($syllabusArray)) {
+                    echo ('<label for="courseTitle">Course Title: </label><br><textarea name="courseTitle" required>');
+                    if(isset($syllabusArray['courseTitle']) && !empty($syllabusArray['courseTitle'])){ echo $syllabusArray['courseTitle']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
 
-                <label for="contactInfo">Contact Information: </label><br>
-                <textarea name="contactInfo" required><?php
-                    if(!empty($_POST['contactInfo'])){ echo $_POST['contactInfo']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
 
-                <label for="officeHours">Office Hours Policy: </label><br>
-                <textarea name="officeHours" required><?php
-                    if(!empty($_POST['officeHours'])){ echo $_POST['officeHours']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
+                    echo ('<label for="contactInfo">Contact Information: </label><br><textarea name="contactInfo" required>');
+                    if(isset($syllabusArray['contactInformation']) && !empty($syllabusArray['contactInformation'])){ echo $syllabusArray['contactInformation']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
 
-                <label for="courseDes">Course Description: </label><br>
-                <textarea name="courseDes" required><?php
-                    if(!empty($_POST['courseDes'])){ echo $_POST['courseDes']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
 
-                <label for="courseGoals">Course Goals: </label><br>
-                <textarea name="courseGoals" required><?php
-                    if(!empty($_POST['courseGoals'])){ echo $_POST['courseGoals']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
+                    echo ('<label for="officeHours">Office Hours Policy: </label><br><textarea name="officeHours" required>');
+                    if(isset($syllabusArray['officeHoursPolicy']) && !empty($syllabusArray['officeHoursPolicy'])){ echo $syllabusArray['officeHoursPolicy']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
 
-                <label for="reqMaterials">Required Materials: </label><br>
-                <textarea name="reqMaterials" required><?php
-                    if(!empty($_POST['reqMaterials'])){ echo $_POST['reqMaterials']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
 
-                <label for="gradingPolicy">Grading Policy: </label><br>
-                <textarea name="gradingPolicy" required><?php
-                    if(!empty($_POST['gradingPolicy'])){ echo $_POST['gradingPolicy']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
+                    echo ('<label for="courseDes">Course Description: </label><br><textarea name="courseDes" required>');
+                    if(isset($syllabusArray['courseDescription']) && !empty($syllabusArray['courseDescription'])){ echo $syllabusArray['courseDescription']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
 
-                <label for="attenPolicy">Attendance Policy: </label><br>
-                <textarea name="attenPolicy" required><?php
-                    if(!empty($_POST['attenPolicy'])){ echo $_POST['attenPolicy']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
 
-                <label for="uniPolicies">University Policies: </label><br>
-                <textarea name="uniPolicies" required><?php
-                    if(!empty($_POST['uniPolicies'])){ echo $_POST['uniPolicies']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
+                    echo ('<label for="courseGoals">Course Goals: </label><br><textarea name="courseGoals" required>');
+                    if(isset($syllabusArray['courseGoals']) && !empty($syllabusArray['courseGoals'])){ echo $syllabusArray['courseGoals']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
 
-                <label for="stuResources">Student Resources: </label><br>
-                <textarea name="stuResources" required><?php
-                    if(!empty($_POST['stuResources'])){ echo $_POST['stuResources']; } else { echo ''; }
-                ?></textarea>
-                <br><br>
 
-                <button type="submit" name="submitData" value="✓">Create Syllabus</button>
-                <br><br>
+                    echo ('<label for="reqMaterials">Required Materials: </label><br><textarea name="reqMaterials" required>');
+                    if(isset($syllabusArray['requiredMaterials']) && !empty($syllabusArray['requiredMaterials'])){ echo $syllabusArray['requiredMaterials']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
+
+
+                    echo ('<label for="gradingPolicy">Grading Policy: </label><br><textarea name="gradingPolicy" required>');
+                    if(isset($syllabusArray['gradingPolicy']) && !empty($syllabusArray['gradingPolicy'])){ echo $syllabusArray['gradingPolicy']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
+
+
+                    echo ('<label for="attenPolicy">Attendance Policy: </label><br><textarea name="attenPolicy" required>');
+                    if(isset($syllabusArray['attendancePolicy']) && !empty($syllabusArray['attendancePolicy'])){ echo $syllabusArray['attendancePolicy']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
+
+
+                    echo ('<label for="uniPolicies">University Policies: </label><br><textarea name="uniPolicies" required>');
+                    if(isset($syllabusArray['universityPolicy']) && !empty($syllabusArray['universityPolicy'])){ echo $syllabusArray['universityPolicy']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
+
+
+                    echo ('<label for="stuResources">Student Resources: </label><br><textarea name="stuResources" required>');
+                    if(isset($syllabusArray['studentResources']) && !empty($syllabusArray['studentResources'])){ echo $syllabusArray['studentResources']; } else { echo ''; }
+                    echo ('</textarea><br><br>');
+
+                    if (isset($syllabusArray['courseTitle'])){
+                        echo('<button type="submit" name="updateSyllabus" value="✓">Update Syllabus</button><br><br>');
+                        echo('<button type="submit" name="deleteSyllabus" value="✓">Delete Syllabus</button><br><br>');
+                    } else {
+                        echo('<button type="submit" name="submitSyllabus" value="✓">Create Syllabus</button><br><br>');
+                    }
+                }
+                ?>
+
             </form>
         </div>
     </body>
