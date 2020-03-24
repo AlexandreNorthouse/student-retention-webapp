cbc73e663b9d66881359dda211bab50cc1536211
brendenjones12 <brendenjones4320@gmail.com>
2020-03-24 17:14:58 -0400

    Did the login and register Tests

---
+++ test/LoginTests.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+    require_once(dirname(__FILE__, 2) . "/logic/Default_Users/Login_Methods.php");
+
+    use PHPUnit\Framework\TestCase;
+
+
+    class LoginMethodsTest extends TestCase
+    {
+
+        public function testAttemptLogin()
+         {
+             $testArray = array(
+                 "Username" => "test",
+                 "Password" => "test"
+             );
+
+             $correctArray = array();
+             $this->assertEquals($correctArray, LoginMethods::attemptLogin($testArray), "They're the same!");
+         }
+
+         public function testSetSessionVariables()
+         {
+             $testArray = array(
+                 "ID" => "test",
+                 "uniID" => "test",
+                 "username" => "test",
+                 "fname"   => "test",
+                 "lname"    => "test",
+                 "isProf"  => "test"
+             );
+
+             LoginMethods::setSessionVariables($testArray);
+
+             $sessionArray = array(
+                 "ID" => $_SESSION['userID'],
+                 "uniID" => $_SESSION['uniID'],
+                 "username" => $_SESSION['username'],
+                 "fname"   => $_SESSION['fName'],
+                 "lname"    => $_SESSION['lName'],
+                 "isProf"  => $_SESSION['isProf']
+             );
+
+             $this->assertEquals($correctArray, $sessionArray, "They're the same!");
+         }
+
+         public function testGetRedirectString()
+         {
+             $testString = "1";
+
+             $correctString = "Location: ../Professor/View_Data.php";
+             $this->assertEquals($correctString, LoginMethods::attemptCourseWithdraw($testString), "They're the same!");
+         }
+    }
+
+
+?>
