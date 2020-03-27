08ac882fdfb4f8ed0a8d945b13b0ab831dc73b3c
Alex Northouse <ajnorthouse@bsu.com>
2020-03-27 10:00:52 -0400

    Add current remote MySQL server variables

--- database/DatabaseVariables.php
+++ database/DatabaseVariables.php
@@ -1,7 +1,17 @@
 <?php
 	// These are the fields needed for setting up the external database connection.
-	$serverName = "localhost:3306";
+
+    /*
+    // Variables for a local database
+    $serverName = "localhost:3306";
 	$username = "root";
 	$password = "";
 	$dbName = "student-engagement-retention-local";
+    */
+
+    // Variables for a remote database
+    $serverName = "remotemysql.com:3306";
+    $username = "EHHA0gzAoD";
+    $password = "wtcS0id97d";
+    $dbName = "EHHA0gzAoD";
 ?>
