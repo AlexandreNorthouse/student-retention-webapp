c22afa78b8f7c8166ece9296d9cbe30b8646eb88
Alex Northouse <ajnorthouse@bsu.com>
2020-04-24 19:34:23 -0400

    Remove remote sql server access variables

--- database/DatabaseVariables.php
+++ database/DatabaseVariables.php
@@ -1,17 +1,7 @@
 <?php
 	// These are the fields needed for setting up the external database connection.
-
-    /*
-    // Variables for a local database
     $serverName = "localhost:3306";
 	$username = "root";
 	$password = "";
 	$dbName = "student-engagement-retention-local";
-    */
-
-    // Variables for a remote database
-    $serverName = "remotemysql.com:3306";
-    $username = "EHHA0gzAoD";
-    $password = "wtcS0id97d";
-    $dbName = "EHHA0gzAoD";
 ?>
